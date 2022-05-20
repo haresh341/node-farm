17c99545cf1cd99b9103f35677c4e949a17a2159
Haresh Prajapati <hp494343@gmail.com>
2022-05-20 15:51:51 +0530

    remove unused code

--- index.js
+++ index.js
@@ -2,41 +2,12 @@
 const fs = require("fs");
 // http module in js
 const http = require("http");
-const path = require("path");
 // url moule in js
 const url = require("url");
 // slugify module from npm
 const slugify = require("slugify");
 // replaceTemplate module
 const replaceTemplate = require(".\\modules\\replaceTemplate");
-// Blocking synchronous way
-// reading the file
-// const textIn = fs.readFileSync(".\\txt\\input.txt", "utf-8");
-// console.log(textIn);
-
-// // writing the file
-// const textOut = `This is what we know about avocado: ${textIn}. \nCreated on ${Date.now()}`;
-// fs.writeFileSync(".\\txt\\output.txt", textOut);
-
-// Non blocking asynchronous way
-// call back hell
-// fs.readFile(".\\txt\\start.txt", "UTF-8", (err, data) => {
-//   fs.readFile(`.\\txt\\${data}.txt`, "UTF8", (err, data2) => {
-//     fs.readFile(`.\\txt\\append.txt`, "UTF8", (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile(
-//         ".\\txt\\final.txt",
-//         `${data2}\n${data3}`,
-//         "utf-8",
-//         (err) => {
-//           console.log("file writing is done.!");
-//         }
-//       );
-//     });
-//   });
-// });
-
-// console.log("completion of reading file");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}\\templates\\template-overview.html`,
